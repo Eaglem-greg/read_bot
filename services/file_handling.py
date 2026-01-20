8a708f467838af3a5b822fd070237b14f0e533b0
Gregory <gregory.novikov@inbox.ru>
2026-01-20 20:22:00 +0300

    Mode services and addition menu button

--- services/file_handling.py
+++ services/file_handling.py
@@ -22,6 +22,19 @@ def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
     return page_text, page_size
 
 def prepare_book(path: str, page_size: int = 1050) -> dict[int, str]:
+    try:
+        with open(file=os.path.normpath(path), mode="r", encoding="UTF-8") as f:
+            text = f.read()
+    except Exception as e:
+        logger.info("Error reading a book: %s", e)
+        raise e
+    start = 0
+    num_of_page = 1
     book = {}
-
-    return book 
+    all_size = len(text)
+    while start < all_size:
+        text_page, size = _get_part_text(text, start, page_size)
+        book[num_of_page] = text_page.strip()
+        num_of_page+=1
+        start += size
+    return book
