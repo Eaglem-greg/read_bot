b4ff0ca2cb39b36a3db0d34655fc04fc3a7a045d
Gregory <gregory.novikov@inbox.ru>
2026-01-21 09:35:40 +0300

    Forward and backward handlers

--- handlers/user.py
+++ handlers/user.py
@@ -44,3 +44,48 @@ async def process_continue_command(message: Message, book: dict, db: dict):
             "forward"
         )
     )
+
+@user_router.message(Command(commands="bookmarks"))
+async def process_bookmarks_command(message: Message, book: dict, db: dict):
+    if db["users"][message.from_user.id]["bookmarks"]:
+        await message.answer(
+            text=LEXICON[message.text],
+            reply_markup=create_bookmarks_keyboard(
+                *db["users"][message.from_user.id]["bookmarks"], book=book
+            )
+        )
+    else:
+        await message.answer(text=LEXICON["no_bookmarks"])
+
+@user_router.callback_query(F.data == 'forward')
+async def process_forward_press(callback: CallbackQuery, book: dict, db: dict):
+    current_page = db["users"][callback.from_user.id]["page"]
+    if current_page < len(book):
+        db["users"][callback.from_user.id]["page"] += 1
+        text = book[current_page+1]
+        await callback.message.edit_text(
+            text=text,
+            reply_markup=create_pagination_keyboard(
+                'backward',
+                f"{current_page+1}/{len(book)}",
+                'forward'
+            )
+        )
+    await callback.answer()
+
+@user_router.callback_query(F.data == 'backward')
+async def process_backward_press(callback: CallbackQuery, book: dict, db: dict):
+    current_page = db["users"][callback.from_user.id]["page"]
+    if current_page > 1:
+        db["users"][callback.from_user.id]["page"] -= 1
+        text = book[current_page-1]
+        await callback.message.edit_text(
+            text=text,
+            reply_markup=create_pagination_keyboard(
+                'backward',
+                f"{current_page-1}/{len(book)}",
+                'forward'
+            )
+        )
+    await callback.answer()
+    
